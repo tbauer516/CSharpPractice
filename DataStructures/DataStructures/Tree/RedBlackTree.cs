1cc1bfd00a51d580299b642e93f0de13f365d8f3
tbauer516 <tcbauer516@gmail.com>
2018-04-11 12:34:02 -0700

    create RedBlackTree stub
    consider making a BalancedTree abstract class

---
+++ DataStructures/DataStructures/Tree/RedBlackTree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Dynamic;
+
+namespace DataStructures.Tree
+{
+    public class RedBlackTree<T> : BinarySearchTree<T> where T : IComparable<T>
+    {
+        public override T Insert(T val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T Remove(T val)
+        {
+            throw new NotImplementedException();
+        }
+        
+        protected class RedBlackNode<T> : BTNode<T> where T : IComparable<T>
+        {
+            public bool Black { get; set; }
+            
+            public RedBlackNode(T val) : base(val)
+            {
+                Black = true;
+            }
+        }
+    }
+}
